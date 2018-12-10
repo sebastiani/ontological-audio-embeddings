47378b603a43c14e14c3971253a422f7f6e37d97
Sebastiani <sebastiani.aguirre@upr.edu>
2018-12-10 12:10:14 -0500

    added fixes

--- src/models/PatchGAN.py
+++ src/models/PatchGAN.py
@@ -30,11 +30,11 @@ class GANLoss(nn.Module):
             return self.loss(input, target_fake_label)
 
 class Generator(nn.Module):
-    def __init__(self, input_dim, num_filters=32, norm_layer=nn.BatchNorm1d, use_sigmoid=False):
+    def __init__(self, input_dim, num_filters=16, norm_layer=nn.BatchNorm1d, use_sigmoid=False):
         super(Generator, self).__init__()
         self.input_dim = input_dim
 
-        kernel_size = 500   # corresponds to 10ms length kernel
+        kernel_size = 160   # corresponds to 10ms length kernel
         pad_size = 1
         nf_mult = 2
 
@@ -42,7 +42,7 @@ class Generator(nn.Module):
 
         self.encoder_layers = []
         previous_mult = input_dim
-        for i in range(0, 5):
+        for i in range(0, 4):
             mult = nf_mult**i
             self.encoder_layers.append(nn.Conv1d(previous_mult, num_filters*mult, kernel_size, stride=stride))
             self.encoder_layers.append(nn.Conv1d(num_filters*mult, num_filters*mult, kernel_size, stride=stride))
@@ -52,15 +52,16 @@ class Generator(nn.Module):
 
         self.decoder_layers = []
 
-        for i in reversed(range(0, 5)):
+
+        for i in reversed(range(0, 3)):
             mult = nf_mult**i
-            self.encoder_layers.append(nn.ConvTranspose1d(previous_mult, num_filters * mult, kernel_size, stride=stride))
-            self.encoder_layers.append(nn.ConvTranspose1d(num_filters * mult, num_filters * mult, kernel_size, stride=stride))
-            self.encoder_layers.append(norm_layer(num_filters * mult))
+            self.decoder_layers.append(nn.ConvTranspose1d(previous_mult, num_filters * mult, kernel_size+1, stride=stride))
+            self.decoder_layers.append(nn.ConvTranspose1d(num_filters * mult, num_filters * mult, kernel_size+1, stride=stride))
+            self.decoder_layers.append(norm_layer(num_filters * mult))
             previous_mult = num_filters * mult
 
+        self.decoder_layers.append(nn.ConvTranspose1d(previous_mult, 1, kernel_size, stride=stride))
         self.decoder_relu = nn.LeakyReLU(0.2, True)
-
 
 
     def forward(self, x):
@@ -68,26 +69,36 @@ class Generator(nn.Module):
         output = x
         count = 0
         print("input ", x.size)
+        print("Num of layers: ", len(self.encoder_layers))
         for i, layer in enumerate(self.encoder_layers):
-            if count < 2:
+            if count <= 2:
                 output = self.encoder_relu(layer(output))
-                print("output", i, ": ", output.size())
+                #print("Encoder Layer", i, ": ", output.size())
                 count += 1
-            else :
+            else:
                 output = layer(output)
-                encoder_outputs.append(output)
-                print(len(encoder_outputs))
+                if len(encoder_outputs) <= 3:
+                    print("appending ", output.size())
+                    encoder_outputs.append(output)
                 count = 0
 
+        print("Num of highways ", len(encoder_outputs))
         count = 0
-        i = 4
+        i = 2
         for layer in self.decoder_layers:
             output = self.decoder_relu(layer(output))
+            print("Decoder layer ", output.size())
             if count == 2:
+                print("Adding ", output.size())
+                print("with ", encoder_outputs[i].size())
                 output = output + encoder_outputs[i]
                 count = 0
                 i -= 1
 
+            count += 1
+
+        output = self.decoder_relu(self.decoder_layers[-1](output))
+        print("Output size ", output.size())
         return output
 
 
@@ -95,7 +106,7 @@ class Discriminator(nn.Module):
     def __init__(self, input_dim, num_filters=64, n_layers=3, norm_layer=nn.BatchNorm1d, use_sigmoid=False):
         super(Discriminator, self).__init__()
 
-        kernel_size = 4
+        kernel_size = 160
         pad_size = 1
         sequence = [
             nn.Conv1d(input_dim, num_filters, kernel_size=kernel_size),
